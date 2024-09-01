73290bb2d910f36db439574608ea0419152e35f1
Furkan Meydan <59588153+frknmydn@users.noreply.github.com>
2024-09-01 16:27:59 +0300

    Delete dist directory

--- dist/src/middleware/user-validator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createUserValidator = void 0;
-const express_validator_1 = require("express-validator");
-exports.createUserValidator = [
-    (0, express_validator_1.body)('name').isString().withMessage('Name must be a string')
-];
